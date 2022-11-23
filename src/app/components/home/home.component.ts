9a5c37e3514e36cbfdb540b8291f175eba162ae1
Inbal Mishory <mishory@gmail.com>
2022-11-23 12:23:52 +0200

    Final touch to navigation and added an image slideshow to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  picIndex: number;
   constructor() { }
 
   ngOnInit(): void {
+    // this.picIndex = 0;
+    // this.carousel();
+  }
+
+  carousel() {
+    // let i;
+    // const imgArr = document.getElementsByClassName("carousel-pic");
+    // for (i = 0; i < imgArr.length; i++) {
+    //   imgArr[i].classList.add('opacity-n');
+    // }
+    // this.picIndex++;
+    // if (this.picIndex > imgArr.length) {
+    //   this.picIndex = 1
+    // }
+    // imgArr[this.picIndex-1].classList.remove('opacity-n');
+    // setTimeout(this.carousel, 3000);
   }
 
 }
