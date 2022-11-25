af9495ebb74e41d8038d1114b85a918c46a85974
Inbal Mishory <mishory@gmail.com>
2022-11-25 18:54:46 +0200

    Removed sensitive information from environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBnXr3Q02h_XpgOKSSZSNrMU2jX-waiIwY",
-    authDomain: "alecangular.firebaseapp.com",
-    databaseURL: "https://alecangular.firebaseio.com",
-    projectId: "alecangular",
-    storageBucket: "alecangular.appspot.com",
-    messagingSenderId: "443167576777",
-    appId: "1:443167576777:web:b6c551feffc3692ff7f318",
-    measurementId: "G-16QF8FMEXN"
-  }
+  production: true
 };
